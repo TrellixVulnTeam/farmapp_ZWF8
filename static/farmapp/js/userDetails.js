73ca941a00a27d5e07f8b28e0f742e8ed431ffa1
kanuru sagar <kanuru.sagar@visiblealpha.com>
2016-06-01 03:04:05 +0530

    url added

--- static/farmapp/js/userDetails.js
+++ static/farmapp/js/userDetails.js
@@ -2,5 +2,5 @@
  * Created by kanuru.sagar on 05/31/16.
  */
 $(document).ready(function () {
- $("#profileDetails").load("../template/ProfileDetails.html");
+ $("#profileDetails").load("../profiledetails");
 });
