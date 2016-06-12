bc2764feeb3fdf6cc2753efe9b606fb3dde784fc
shravanchandra <shrvn.chandra@gmail.com>
2016-06-12 14:16:57 +0530

    changes to add token

--- static/farmapp/js/Registration.js
+++ static/farmapp/js/Registration.js
@@ -6,7 +6,10 @@ function SaveUserDetails(){
             url: "http://urconnected.in/api/accounts/",
             type: "POST",
             async: false,
-            data: data,
+            data:  {
+            'content': data,
+            'csrfmiddlewaretoken': '{{ csrf_token }}',
+            },
             dataType: "json",
             contenttype: "application/json; charset=utf-8",
             success: function (jsonData) {
@@ -16,10 +19,11 @@ function SaveUserDetails(){
                 alert('Error occurred while  saving user details.\n Please Contact Appssupport.' + jqXHR.statusText);
             }
         });
-}
 // $.post('/api/accounts/', data).done(function(data){
 //     console.log(data);
 // });
+}
+
 
 /**
  * Created by kanuru.sagar on 02/29/16.
