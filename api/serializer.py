ce6bd14d5739faa42825524ed43a03720577c083
shravanchandra <shrvn.chandra@gmail.com>
2016-06-16 21:03:39 +0530

    changes for registration

--- api/serializer.py
+++ api/serializer.py
@@ -17,7 +17,16 @@ class UserSerializer(serializers.ModelSerializer):
         }
 
     def create(self, validated_data):
-        user = get_user_model().objects.create_user(**validated_data)
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
+        )
+
+        user.set_password(validated_data['password'])
+        user.save()
+
         return user
 
     def update(self, instance, validated_data):
