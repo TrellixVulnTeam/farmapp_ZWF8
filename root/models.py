48bd3bdb42e52499e42cc1c3853834d7cfdf85e5
shravanchandra <shrvn.chandra@gmail.com>
2016-08-06 12:43:44 +0530

    change

--- root/models.py
+++ root/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-def upload_to(instance):
+def upload_to(instance, change):
     return 'Images/{}/{}'.format(instance.__class__.__name__, instance.id)
 
 class State(models.Model):
