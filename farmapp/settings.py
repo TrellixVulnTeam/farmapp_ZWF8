d38f6ff624a0918d3d32910eef8ae9ad33c53150
shravanchandra <shrvn.chandra@gmail.com>
2016-08-20 16:50:17 +0530

    adding storages

--- farmapp/settings.py
+++ farmapp/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'storages',
     'corsheaders',
     'django_extensions',
     # 'crispy-forms',
